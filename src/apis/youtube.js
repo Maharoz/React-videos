93cfd159194072c3264c6faead526c79c1f617f4
maharoz <mahruj66@gmail.com>
2019-06-27 20:31:36 +0600

    Searching for videos

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
 const KEY = "AIzaSyCuagvc-REgYQqveFCAiqMGDRZ9yHkTsC8";
+
+
+export default axios.create({
+    baseURL:'https://www.googleapis.com/youtube/v3',
+    params:{
+        part:'snippet',
+        maxResults:5,
+        key:KEY
+    }
+});
+
