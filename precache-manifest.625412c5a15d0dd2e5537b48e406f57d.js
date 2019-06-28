1c46b708a03d171801d8c852ce0c2bc14eae7898
maharoz <mahruj66@gmail.com>
2019-06-28 18:52:23 +0600

    Updates

---
+++ precache-manifest.625412c5a15d0dd2e5537b48e406f57d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389b02cbb980392d4b680a271d4ae3a4",
+    "url": "/React-videos/index.html"
+  },
+  {
+    "revision": "cecc7583e38b7f98b6f4",
+    "url": "/React-videos/static/css/main.83681c6a.chunk.css"
+  },
+  {
+    "revision": "01f001c735625cba4d20",
+    "url": "/React-videos/static/js/2.fc36bde1.chunk.js"
+  },
+  {
+    "revision": "cecc7583e38b7f98b6f4",
+    "url": "/React-videos/static/js/main.4916965b.chunk.js"
+  },
+  {
+    "revision": "b592ff2ea826ad480bd0",
+    "url": "/React-videos/static/js/runtime~main.d17b82ec.js"
+  }
+]);
